34ed100998f644d5ef18e4915f319738106e803d
Muhyun-Kim <azs030369@gmail.com>
2023-02-05 15:05:01 +0900

    create db

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [post, setPost] = useState("");
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await addDoc(collection(db, "post"), {
+      post,
+      createdAt: Date.now(),
+    });
+    setPost("");
+  };
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+    setPost(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={post}
+          onChange={onChange}
+          type="text"
+          placeholder="what's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,9 +12,10 @@ const firebaseConfig = {
   projectId: "drinker-8c793",
   storageBucket: "drinker-8c793.appspot.com",
   messagingSenderId: "628507954444",
-  appId: "1:628507954444:web:c954e33a5e39cc73884d29"
+  appId: "1:628507954444:web:c954e33a5e39cc73884d29",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const db = getFirestore(app);
